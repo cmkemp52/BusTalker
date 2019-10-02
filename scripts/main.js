1a93960febe0d9ea53cf82293978f9d0713d8aea
Christopher Kemp <cmkemp52@gmail.com>
2019-10-02 12:22:35 -0400

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -25,7 +25,7 @@ function updateMap(){
                                         mymap.removeLayer(tlayer);
                                     }
                                 }});
-                            let nIcon = L.icon({iconUrl:"images/busnorth.png",iconSize:[50,50],iconAnchor:[0,0],popupAnchor:[25,10]});
+                            let nIcon = L.icon({iconUrl:"images/busnorth.png",iconSize:[50,50],iconAnchor:[25,25],popupAnchor:[0,-10]});
                             marker = L.marker([jsondata[bus].LATITUDE, jsondata[bus].LONGITUDE],{icon:nIcon}).addTo(mymap);
                             if(jsondata[bus].ADHERENCE >0){
                                 marker.bindPopup(`<b>Bus Rt: ${jsondata[bus].ROUTE} Bus ID:${jsondata[bus].VEHICLE}</b><br> Currently ${jsondata[bus].ADHERENCE} minute\(s\) ahead of schedule`);
@@ -44,7 +44,7 @@ function updateMap(){
                                         mymap.removeLayer(tlayer);
                                     }
                             }});
-                            let sIcon = L.icon({iconUrl:"images/bussouth.png",iconSize:[50,50],iconAnchor:[0,0],popupAnchor:[25,10]});
+                            let sIcon = L.icon({iconUrl:"images/bussouth.png",iconSize:[50,50],iconAnchor:[25,25],popupAnchor:[0,-10]});
                             marker = L.marker([jsondata[bus].LATITUDE, jsondata[bus].LONGITUDE],{icon:sIcon}).addTo(mymap);
                             if(jsondata[bus].ADHERENCE >0){
                                 marker.bindPopup(`<b>Bus Rt: ${jsondata[bus].ROUTE} Bus ID:${jsondata[bus].VEHICLE}</b><br> Currently ${jsondata[bus].ADHERENCE} minute\(s\) ahead of schedule`);
@@ -63,7 +63,7 @@ function updateMap(){
                                         mymap.removeLayer(tlayer);
                                     }
                             }});
-                            let eIcon = L.icon({iconUrl:"images/buseast.png",iconSize:[50,50],iconAnchor:[0,0],popupAnchor:[25,10]});
+                            let eIcon = L.icon({iconUrl:"images/buseast.png",iconSize:[50,50],iconAnchor:[25,25],popupAnchor:[0,-10]});
                             marker = L.marker([jsondata[bus].LATITUDE, jsondata[bus].LONGITUDE],{icon:eIcon}).addTo(mymap);
                             if(jsondata[bus].ADHERENCE >0){
                                 marker.bindPopup(`<b>Bus Rt: ${jsondata[bus].ROUTE} Bus ID:${jsondata[bus].VEHICLE}</b><br> Currently ${jsondata[bus].ADHERENCE} minute\(s\) ahead of schedule`);
@@ -82,7 +82,7 @@ function updateMap(){
                                         mymap.removeLayer(tlayer);
                                     }
                             }});
-                            let wIcon = L.icon({iconUrl:"images/buswest.png",iconSize:[50,50],iconAnchor:[0,0],popupAnchor:[25,10]});
+                            let wIcon = L.icon({iconUrl:"images/buswest.png",iconSize:[50,50],iconAnchor:[25,25],popupAnchor:[0,-10]});
                             marker = L.marker([jsondata[bus].LATITUDE, jsondata[bus].LONGITUDE],{icon:wIcon}).addTo(mymap);
                             if(jsondata[bus].ADHERENCE >0){
                                 marker.bindPopup(`<b>Bus Rt: ${jsondata[bus].ROUTE} Bus ID:${jsondata[bus].VEHICLE}</b><br> Currently ${jsondata[bus].ADHERENCE} minute\(s\) ahead of schedule`);
@@ -131,25 +131,18 @@ function addIcon(icon) {
     if (icon == 'clear-day') {
     iconElement.innerHTML = `<src="images/sun.png">`;
     weatherDiv.append(iconElement);
-    console.log('worked')
     }
     if(icon == "partly-cloudy-day"){
     iconElement.innerHTML = `<src="images/cloudy.png">`;
     weatherDiv.append(iconElement);
-    console.log('worked')
     }
     if(icon == "fog"){
     iconElement.innerHTML = `src="images/foggy.png">`;
     weatherDiv.append(iconElement);
-    console.log('worked')
     }
     if(icon == "partly-cloudy-night") {
     iconElement.innerHTML = `<img src="images/sun-weather.gif" height="100px" width=“200px" class='sun'>`;
     weatherDiv.append(iconElement);
-    console.log('worked')
-    }
-    else {
-        console.log('error')
     }
 }
 
@@ -157,9 +150,6 @@ function getWeather(icon) {
     const URL = `https://api.darksky.net/forecast/1010d61071e3e5e3e99eed847a82272c/33.7490,-84.3880`;
 
     get(URL).then(function(response) {
-        console.log(response.currently.icon);
-        console.log(response.currently.time);
-        console.log(response)
         addIcon(response.minutely.icon);
         addTemp(response.currently.temperature);
         addSummary(response.hourly.summary);
